f9093916c4f0baf6e5c451d37a8e4828b4812cdd
raqulec <krzysztofrak90@wp.pl>
2019-01-03 01:26:49 +0100

    create layout

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+global.jQuery = require('jquery');
+require('bootstrap');
+
+ReactDOM.render(<App />, document.getElementById('root'));
